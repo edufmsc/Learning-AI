362869a6e0a47925a53ac32315db59847b2ece68
edufmsc <edu.fmsc@gmail.com>
2026-09-01 13:30:55 +0800

    Switch frontend to new GAS Web App deployment

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.AI_LMS_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbzaO0xFgm15VER42cB1uenZ1kT_oHXOHwbIagDw5h9pvp218wbcObfAhahO2Lb6a6Gn5A/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbzFHp4ZOqhtmZZgvqYRtLfdHgsA6vHyt_HmSCqUXs2QqK0rIVzV8xpN1Z1pPgTTLul_/exec',
   GOOGLE_CLIENT_ID: '731637287753-diajenfrr7p0bs43isijf1t40u7a07m6.apps.googleusercontent.com',
   SITE_ORIGIN: 'https://edufmsc.github.io'
 };
