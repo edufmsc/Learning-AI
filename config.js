20f5fc0c28a734f64cb9fa45544e5570a2e11481
edufmsc <edu.fmsc@gmail.com>
2026-09-01 09:39:55 +0800

    Add frontend configuration

---
+++ config.js
@@ -0,0 +1,10 @@
+window.AI_LMS_CONFIG = {
+  // 部署 GAS 後，把 /exec 網址貼在這裡。
+  API_URL: '',
+
+  // Google Cloud Console 建立「網頁應用程式」OAuth 2.0 Client ID 後貼在這裡。
+  GOOGLE_CLIENT_ID: '',
+
+  // GitHub Pages 正式網址。
+  SITE_ORIGIN: 'https://edufmsc.github.io'
+};
