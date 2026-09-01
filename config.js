9317863d46997d0344b1abe5f6aa7bf90565a426
edufmsc <edu.fmsc@gmail.com>
2026-09-01 10:48:27 +0800

    Connect frontend to Google OAuth and Apps Script API

--- config.js
+++ config.js
@@ -1,10 +1,5 @@
 window.AI_LMS_CONFIG = {
-  // 部署 GAS 後，把 /exec 網址貼在這裡。
-  API_URL: '',
-
-  // Google Cloud Console 建立「網頁應用程式」OAuth 2.0 Client ID 後貼在這裡。
-  GOOGLE_CLIENT_ID: '',
-
-  // GitHub Pages 正式網址。
+  API_URL: 'https://script.google.com/macros/s/AKfycbzaO0xFgm15VER42cB1uenZ1kT_oHXOHwbIagDw5h9pvp218wbcObfAhahO2Lb6a6Gn5A/exec',
+  GOOGLE_CLIENT_ID: '731637287753-diajenfrr7p0bs43isijf1t40u7a07m6.apps.googleusercontent.com',
   SITE_ORIGIN: 'https://edufmsc.github.io'
 };
